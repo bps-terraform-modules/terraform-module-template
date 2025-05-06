ddc5f007fa17472d8872f40eeb0cdb3b4d1d09e8
Jeff Beagley <jbeagley@basspro.com>
2025-05-06 09:49:14 -0500

    fix solution providing multiple runners in the same cluster

--- main.tf
+++ main.tf
@@ -15,12 +15,14 @@ data "azurerm_key_vault" "bps-devops-kv" {
 # PAT from github, that is stored in our azure keyvault. The user that generates this PAT needs to have access to appropriate repo/org
 # https://github.com/settings/tokens
 data "azurerm_key_vault_secret" "repo-pat" {
-  name         = var.key_vault_name
+  for_each     = { for repo in var.github_urls : repo.key_vault_name => repo }
+  name         = each.value.key_vault_name
   key_vault_id = data.azurerm_key_vault.bps-devops-kv.id
 }
 
 resource "helm_release" "helm-arc" {
-  name             = "actions-runner-controller"
+  for_each         = { for repo in var.github_urls : repo.name => repo }
+  name             = "actions-runner-controller-${var.runner_name}-${each.value.name}"
   repository       = "oci://ghcr.io/actions/actions-runner-controller-charts"
   chart            = "gha-runner-scale-set-controller"
   create_namespace = true
@@ -28,14 +30,23 @@ resource "helm_release" "helm-arc" {
 
   set {
     name  = "githubConfigSecret.github_token"
-    value = data.azurerm_key_vault_secret.repo-pat.value
+    value = data.azurerm_key_vault_secret.repo-pat[each.value.key_vault_name].value
+  }
 
+  set {
+    name  = "serviceAccount.create"
+    value = true
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "sa-${each.value.name}"
   }
 
 }
 
 resource "helm_release" "helm-arc-runners" {
-  for_each = { for release in var.github_urls : release.name => release }
+  for_each   = { for repo in var.github_urls : repo.name => repo }
   name       = "${var.runner_name}-${each.value.name}"
   repository = "oci://ghcr.io/actions/actions-runner-controller-charts"
   chart      = "gha-runner-scale-set"
@@ -51,31 +62,26 @@ resource "helm_release" "helm-arc-runners" {
 
   set {
     name  = "githubConfigSecret.github_token"
-    value = data.azurerm_key_vault_secret.repo-pat.value
+    value = data.azurerm_key_vault_secret.repo-pat[each.value.key_vault_name].value
 
   }
 
   set {
-    name  = "template.controllerServiceAccount.namespace"
+    name  = "controllerServiceAccount.namespace"
     value = "github"
 
   }
 
   set {
-    name  = "template.controllerServiceAccount.name"
-    value = "actions-runner-controller-gha-rs-controller"
-
-  }
-
-  set {
-    name  = "template.spec.serviceAccountName"
-    value = "actions-runner-controller-gha-rs-controller"
+    name  = "controllerServiceAccount.name"
+    value = "sa-${each.value.name}"
 
   }
 
 }
 
 resource "helm_release" "helm-arc-runners-customrole" {
+  for_each         = { for repo in var.github_urls : repo.name => repo }
   name             = "github-actions-roles"
   chart            = "${path.module}/github-actions-roles" # Specify the relative path to the Helm chart within the repo
   namespace        = "github"
@@ -84,7 +90,7 @@ resource "helm_release" "helm-arc-runners-customrole" {
 
   set {
     name  = "service_account.name"
-    value = "actions-runner-controller-gha-rs-controller"
+    value = "sa-${each.value.name}"
 
   }
 
--- variables.tf
+++ variables.tf
@@ -1,8 +1,3 @@
-variable "key_vault_name" {
-  type        = string
-  description = "Name of secret in the bps devops key vault"
-}
-
 variable "runner_name" {
   type        = string
   description = "Name of the runner scale set that will be used for targeting, ie a cluster name"
@@ -10,8 +5,9 @@ variable "runner_name" {
 
 variable "github_urls" {
   type = list(object({
-    name = string
-    url  = string
+    name           = string
+    url            = string
+    key_vault_name = string
   }))
   description = "URL of organization or repo, ie https://github.com/bps-eai"
 }
