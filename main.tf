7922fdd2b0b3ca9e86c0e19fa22b1127f2a92626
Jeff Beagley <jbeagley@basspro.com>
2025-05-16 15:47:33 -0500

    bump chart version for custom roles

--- main.tf
+++ main.tf
@@ -92,7 +92,7 @@ resource "helm_release" "helm-arc-runners-customrole" {
   name             = "github-actions-roles"
   chart            = "${path.module}/github-actions-roles" # Specify the relative path to the Helm chart within the repo
   namespace        = "github"
-  version          = "0.0.8"
+  version          = "0.0.9"
   create_namespace = true
 
   set {
